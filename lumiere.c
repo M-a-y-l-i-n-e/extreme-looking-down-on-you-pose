392f6b4ab1bb8cee40fcff05dff6c1865d045022
test <test@localhost.localdomain>
2019-12-05 07:09:11 -0800

    touch

--- lumiere.c
+++ lumiere.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 void afficherfl(){
-	printf("La fete des lumieres commence aujourd'hui, je vais y aller  \n");
+	printf("La fete des lumieres commence aujourd'hui, je vais y aller demain \n");
 }
